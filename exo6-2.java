bf66d11e87ce193d15fe4a69f4f84fc4a590a058
RenéDEK <gringok360@gmail.com>
2023-11-08 12:55:14 +0000

    Second Commit

---
+++ exo6-2.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Voyelles{
+  
+    public static void main (String[] args){
+      String[] sixVoyelles = new String[6];
+        sixVoyelles[0] = "a";
+        sixVoyelles[1] = "e";
+        sixVoyelles[2] = "u";
+        sixVoyelles[3] = "i";
+        sixVoyelles[4] = "o";
+        sixVoyelles[5] = "y";
+
+      System.out.println(Arrays.toString(sixVoyelles));
+    // System.out.println(sixVoyelles);
+    }
+}
+/*String[] helloWorld = {"Hello", "World"};
+System.out.println(helloWorld);
+System.out.println(Arrays.toString(helloWorld));
+
+//is
+
+[Ljava.lang.String;@45a877
+[Hello, World]
+*/
