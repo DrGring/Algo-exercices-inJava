d3c954e479356bea0928eb84794879eb4ccae598
RenéDEK <gringok360@gmail.com>
2023-11-08 09:45:14 +0000

    First commit

---
+++ exo6-1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class numberAZero {
+  
+    public static void main (String[] args){
+      int [] valeursNum = new int [7];
+      
+      for (int i = 0; i<valeursNum.length; i++) {
+        valeursNum[i]=0;
+      }
+      System.out.println(Arrays.toString(valeursNum));
+    }
+}
