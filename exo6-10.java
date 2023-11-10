18190a8c6479cdfd65fc9d6f3a4c96a30130c22e
RenéDEK <gringok360@gmail.com>
2023-11-10 16:16:32 +0000

    Commit de l'algo qui calcule la somme de deux tableaux et l'affiche a la fin

---
+++ exo6-10.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] firstTab = {4, 8, 7, 9, 1, 5, 4, 6};
+        int [] secondTab = {7, 6, 5, 2, 1, 3, 7, 4};
+        int [] sumTab = new int [8];
+
+        for (int i=0; i<firstTab.length; i++) {
+            sumTab[i] = firstTab[i]+secondTab[i];
+        }
+
+        System.out.println("La somme est égale à : "+Arrays.toString(sumTab));
+    }
+}
